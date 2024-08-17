4194a7f78b11aac15279f4ddf4a72407b61c96e7
Promit Biswas <promit.biswas0710@gmail.com>
2024-08-17 23:11:56 +0530

    Prevent back navigation after logout 1

--- PHP Connectivity/SignOut.php
+++ PHP Connectivity/SignOut.php
@@ -3,6 +3,10 @@ session_start();
 session_unset();
 session_destroy();
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
